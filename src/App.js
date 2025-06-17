71ee3e866083a9ce9245878539bd4f7a3a0fd07f
Charlotte-82 <charlotteregis1@gmail.com>
2025-06-17 10:53:13 +0200

    start home page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
 import Program from "./pages/Program";
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,11 @@
+import LogoHeaderRouge from "../assets/images/logos/Logo rond ticket rouge.png";
+
 function Header() {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <img className="logoHeader" src={LogoHeaderRouge} />
+    </div>
+  );
 }
 
 export default Header;
