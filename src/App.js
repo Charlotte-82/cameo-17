9548bc8edf06ef2d47aec5e9cbb569a737c4da8b
Charlotte-82 <charlotteregis1@gmail.com>
2025-06-17 10:35:09 +0200

    routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+import Program from "./pages/Program";
+import Partnership from "./pages/Partnership";
+import Shop from "./pages/Shop";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Routes></Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/boutique" element={<Shop />}></Route>
+        <Route path="/blog" element={<Blog />}></Route>
+        <Route path="/agenda" element={<Program />}></Route>
+        <Route path="/partenaires" element={<Partnership />}></Route>
+      </Routes>
+      <Footer />
     </div>
   );
 }
