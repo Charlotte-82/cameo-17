9acd5899f1973967ca9925fc3d330482898d0e78
Charlotte-82 <charlotteregis1@gmail.com>
2025-06-30 01:51:14 +0200

    video config

--- src/components/PatisserieSemaine.js
+++ src/components/PatisserieSemaine.js
@@ -47,7 +47,8 @@ function PatisserieSemaine() {
       );
     }
 
-    const videoFileMatch = url.match(/\.(mp4|webm|ogg|mov)$/i);
+    const videoFileMatch =
+      url.includes("cloudinary.com") || url.match(/\.(mp4|webm|ogg|mov)$/i);
     if (videoFileMatch) {
       return (
         <video controls className="videoDirect">
